87d9968ea9157e3a1496188c0ac537217847a2d3
Glen Keane <glenkeane.94@gmail.com>
2016-04-19 11:28:55 +0100

    removed packages, added makefile

--- Sources/main.swift
+++ Sources/main.swift
@@ -3,12 +3,25 @@ import KituraNet
 import KituraSys
 
 let router = Router()
+let port = 8090
 
-router.get("/") {
-request, response, next in
+router.get("/") { request, response, next in
+    print("got a request")
     response.send("Hello, World!")
     next()
 }
 
-let server = HttpServer.listen(8090, delegate: router)
+router.get("/api/song") { req, res, next in
+
+}
+
+router.get("/api/song/:id") { req, res, next in
+
+}
+
+router.get("/api/song/:id/") { req, res, next in
+
+}
+
+let server = HttpServer.listen(port, delegate: router)
 Server.run()
